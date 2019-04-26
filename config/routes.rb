036c5fe5c93440667d1392edb258af07fd99db0e
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-04-26 11:31:02 -0500

    changed root, added show route for canvas
    - root:
      - changed action to go to index instead of show for canvases cont
      - (2)
    
    - show_canvas_route:
      - routes to GET request
      - url is 'grafitti/:id' where id is a url param used to build it
      - routes to show action of canvas
      - route helper is canvas (ex: canvas_path(@canvas))
      - (3)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'canvases#show'
+  root 'canvases#index'
+  get 'grafitti/:id', to: 'canvases#show', as: 'canvas'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount ActionCable.server => '/cable'
 end

34e9dc9caebbe35b9bece4aef83c877ee99c6e05
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-04-30 20:06:13 -0500

    changed details route of a canvas
    - switched from using id exclusively to using id and slug in url
    generation (3)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'canvases#index'
-  get 'grafitti/:id', to: 'canvases#show', as: 'canvas'
+  get 'canvases/:id/:slug', to: 'canvases#show', as: 'canvas'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount ActionCable.server => '/cable'
 end

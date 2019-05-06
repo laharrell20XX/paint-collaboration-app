44993da72908156bb7c5a2ea454174d78b285d48
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-05-06 02:44:19 -0500

    added throwaway channel for initial syncing
    - for latecomers to the canvas
    - send_sync_request
      - takes data['request_to_sync'] and sends it through the
      appropriate paint_channel
      - only happens if there is currently 3 or more connections on the
      server
      - aka when two people or more are connected to a canvas channel
      + 1 or more initial_canvas_sync_channel connections
      - closes the channel otherwise

---
+++ app/channels/initial_canvas_sync_channel.rb
@@ -0,0 +1,21 @@
+class InitialCanvasSyncChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "initial_canvas_sync_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def send_sync_request(data)
+      if ActionCable.server.connections.length <= 3
+        close_channel
+      else 
+        ActionCable.server.broadcast "paint_channel:#{data['canvas_name']}", request_to_sync: data['request_to_sync']
+      end
+  end
+
+  def close_channel
+    stop_all_streams
+  end
+end

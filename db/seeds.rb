79ebe02c42c059f03aeb6423c6ee1a665acf22af
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-05-06 08:42:36 -0500

    data to seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Canvas.create(name: 'My Canvas', slug: to_slug('My Canvas'))
+Canvas.create(name: 'Your Canvas', slug: to_slug('Your Canvas'))

b18dabc6f4a6a91797bc90245ed340c9173227a3
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-05-08 08:44:56 -0500

    initialized redis heroku setup for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.web_socket_server_url = "wss://paint-collab.herokuapp.com/cable" 
+
+  config.action_cable.allowed_request_origins = ['https://paint-collab.herokuapp.com', 'http://paint-collab.herokuapp.com']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
